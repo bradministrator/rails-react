ff256a36e9f2aa7626f6282cfc35e1e878e0cddc
bradministrator <brad.cloutier.personal@gmail.com>
2021-08-20 19:36:24 -0500

    first stage of composition vs inheritance

--- app/javascript/packs/cvi.jsx
+++ app/javascript/packs/cvi.jsx
@@ -2,19 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-// for syling:
-import './styles/cvi.scss'
-
-
 const root = document.getElementById('cvi')
 
-function ItWorks(props){
+function FancyBorder(props){
   return (
-    <h1>It Works!</h1>
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
   );
 }
 
+function WelcomeDialog(props){
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="DialogMessage">
+        Thank you for visiting our spacecraft!
+      </p>
+    </FancyBorder>
+  );
+}
+
+console.log('mounting component');
+
+
 ReactDOM.render(
-  <ItWorks />,
+  <WelcomeDialog />,
   root
 );
